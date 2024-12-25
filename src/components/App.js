7ff547d0e8672aaef34f6ae2cd08ed90add1c029
Ravisharma-1999 <ravi829144@gmail.com>
2024-12-25 18:46:18 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,13 @@ import './../styles/App.css';
 const App = () => {
   return (
     <div>
+    const name = () =>{
+    return hello
+
+      )
+  }
+  <p>Enter your Name</p>
+  <input>{this.name}</input>
         {/* Do not remove the main div */}
     </div>
   )
